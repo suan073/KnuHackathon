e2987e2ead75df7f73927c2f61ef4fcbc98d9f1f
miloul <milouw@naver.com>
2021-07-20 21:42:18 +0900

    first

---
+++ app/src/main/java/com/example/bottonnav/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.bottonnav;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //viewpager.adapter=
+    }
+
+    //inner class
+}
