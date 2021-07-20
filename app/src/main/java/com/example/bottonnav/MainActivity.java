a69d8f4c70045ba5bd22681bd0da14e8c226b941
{milouw56@gmail.com} <milouw@naver.com>
2021-07-21 02:33:47 +0900

    second

--- app/src/main/java/com/example/bottonnav/MainActivity.java
+++ app/src/main/java/com/example/bottonnav/MainActivity.java
@@ -7,22 +7,68 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private BottomNavigationView mBottomNV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar tb=(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
 
-        //viewpager.adapter=
+        mBottomNV = findViewById(R.id.home);
+        mBottomNV.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() { //NavigationItemSelecte
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                BottomNavigate(menuItem.getItemId());
+                return true;
+            }
+        });
+        mBottomNV.setSelectedItemId(R.id.home);
     }
+    private void BottomNavigate(int id) {  //BottomNavigation 페이지 변경
+        String tag = String.valueOf(id);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-    //inner class
+        Fragment currentFragment = fragmentManager.getPrimaryNavigationFragment();
+        if (currentFragment != null) {
+            fragmentTransaction.hide(currentFragment);
+        }
+
+        Fragment fragment = fragmentManager.findFragmentByTag(tag);
+        if (fragment == null) {
+            if (id == R.id.home) {
+                fragment = new FragmentPage1();
+
+            } else if (id == R.id.notice){
+
+                fragment = new FragmentPage2();
+            }else {
+                fragment = new FragmentPage3();
+            }
+
+            fragmentTransaction.add(R.id.frame_container, fragment, tag);
+        } else {
+            fragmentTransaction.show(fragment);
+        }
+
+        fragmentTransaction.setPrimaryNavigationFragment(fragment);
+        fragmentTransaction.setReorderingAllowed(true);
+        fragmentTransaction.commitNow();
+
+
+    }
+        //viewpager.adapter=
 }
