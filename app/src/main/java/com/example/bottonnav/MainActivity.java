b24f82bbc712ae1e9d8ad7312fcf28d9e631cf3f
{milouw56@gmail.com} <milouw@naver.com>
2021-07-21 13:26:03 +0900

    bar

--- app/src/main/java/com/example/bottonnav/MainActivity.java
+++ app/src/main/java/com/example/bottonnav/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.bottonnav;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
+
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,10 +16,9 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
-    private BottomNavigationView mBottomNV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +28,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar tb=(Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(tb);
 
-        mBottomNV = findViewById(R.id.home);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24);
+
+        BottomNavigationView mBottomNV = findViewById(R.id.nav_m);
         mBottomNV.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() { //NavigationItemSelecte
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
@@ -37,6 +41,34 @@ public class MainActivity extends AppCompatActivity {
         });
         mBottomNV.setSelectedItemId(R.id.home);
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //return super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.top_menu, menu);
+        return true;
+    }
+
+    //추가된 소스, ToolBar에 추가된 항목의 select 이벤트를 처리하는 함수
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            //case R.id.set:
+               // Toast.makeText(getApplicationContext(), "환경설정 버튼 클릭됨", Toast.LENGTH_LONG).show();
+                //return true;
+            case R.id.ala:
+                // User chose the "Settings" item, show the app settings UI...
+                Toast.makeText(getApplicationContext(), "알람 버튼 클릭됨", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
     private void BottomNavigate(int id) {  //BottomNavigation 페이지 변경
         String tag = String.valueOf(id);
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -52,10 +84,10 @@ public class MainActivity extends AppCompatActivity {
             if (id == R.id.home) {
                 fragment = new FragmentPage1();
 
-            } else if (id == R.id.notice){
+            } else if (id == R.id.notice) {
 
                 fragment = new FragmentPage2();
-            }else {
+            } else {
                 fragment = new FragmentPage3();
             }
 
@@ -70,5 +102,4 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-        //viewpager.adapter=
 }
