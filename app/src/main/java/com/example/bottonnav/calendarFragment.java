9e31248403c7314e8697933e763fe79287a0e9c3
suan073 <xhfntndls82@knu.ac.kr>
2021-07-23 08:38:09 +0900

    cv 선택에 따른 날짜 변수 set 구현

--- app/src/main/java/com/example/bottonnav/calendarFragment.java
+++ app/src/main/java/com/example/bottonnav/calendarFragment.java
@@ -1,6 +1,7 @@
 package com.example.bottonnav;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -234,6 +235,7 @@ public class calendarFragment extends Fragment {
     public void onActivityCreated(Bundle b){
         super.onActivityCreated(b);
         btn_add = (Button) getView().findViewById(R.id.btn_add);
+        cv = (CalendarView) getView().findViewById(R.id.cv);
 
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -243,5 +245,18 @@ public class calendarFragment extends Fragment {
             }
         });
 
+
+        cv.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(getActivity(), "!", Toast.LENGTH_LONG).show();
+                //String date = (month+1) + "/" + dayOfMonth + "/" + year;
+
+                theDay = dayOfMonth;
+                theMonth = month;
+
+                //Toast.makeText(getApplicationContext(), date, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
