cf7ffb317b059622629ac4533de4ee64f151515b
ahnl1297 <87692885+ahnl1297@users.noreply.github.com>
2021-07-22 16:33:41 +0900

    Add files via upload
    빠진거

---
+++ webView.java
@@ -0,0 +1,50 @@
+package com.example.bottonnav;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageButton;
+
+public class webView extends AppCompatActivity {
+
+    private WebView webview;
+    Intent intent = getIntent();
+    private String url = intent.getStringExtra("url");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        webview = (WebView)findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl(url);
+        webview.setWebChromeClient(new WebChromeClient());
+        webview.setWebViewClient(new WebViewClientClass());
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webview.canGoBack()){
+            webview.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class WebViewClientClass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+    }
+}
