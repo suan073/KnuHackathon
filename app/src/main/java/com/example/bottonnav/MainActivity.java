f3dedc32febe4b045ebfbf00c268c660b5c1cd11
{milouw56@gmail.com} <milouw@naver.com>
2021-07-22 21:04:50 +0900

    검색

--- app/src/main/java/com/example/bottonnav/MainActivity.java
+++ app/src/main/java/com/example/bottonnav/MainActivity.java
@@ -36,14 +36,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener {
-
-    Button fil;
-    Dialog dialog;
-    private TextView tv_id, tv_pw;
-    private TextView monthYearText;
-    private RecyclerView calendarRecyclerView;
-    private LocalDate selectedDate;
+public class MainActivity extends AppCompatActivity {
 
 
     @Override
@@ -51,9 +44,6 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initWidgets();
-        selectedDate = LocalDate.now();
-        setMonthView();
 
         Toolbar tb=(Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(tb);
@@ -72,44 +62,8 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
         mBottomNV.setSelectedItemId(R.id.home);
 
 
-        RadioGroup ragroup=findViewById(R.id.rbgroup);
-        RadioButton all = findViewById(R.id.rb_all);
-       // cust=findViewById(R.id.rb_custom);
-         //all.setChecked(true);
-
-        dialog =new Dialog(MainActivity.this);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(R.layout.dialog);
-
-        findViewById(R.id.filter).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDialog();
-            }
-        });
     }
 
-    public void showDialog(){
-        dialog.show();
-
-        Button nob=dialog.findViewById(R.id.nob);
-        Button yesb=dialog.findViewById(R.id.yesb);
-        nob.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view){
-                dialog.dismiss();
-            }
-        });
-
-        yesb.findViewById(R.id.yesb).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 원하는 기능 구현
-                finish();           // 앱 종료
-            }
-        });
-
-    }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         //return super.onCreateOptionsMenu(menu);
@@ -172,67 +126,4 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
 
     }
 
-    //calendar 전용 함수들
-    private void initWidgets() {
-        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
-        monthYearText = findViewById(R.id.monthYearTV);
-    }
-
-
-
-    private void setMonthView() {
-        monthYearText.setText(monthYearFromDate(selectedDate));
-        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
-
-        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
-        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
-        calendarRecyclerView.setLayoutManager(layoutManager);
-        calendarRecyclerView.setAdapter(calendarAdapter);
-    }
-
-
-
-    private ArrayList<String> daysInMonthArray(LocalDate date) {
-        ArrayList<String> daysInMonthArray = new ArrayList<>();
-        YearMonth yearMonth = YearMonth.from(date);
-        int daysInMonth = yearMonth.lengthOfMonth();
-        LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
-        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
-        for( int i = 1; i<= 42; i++){
-            if(i<= dayOfWeek || i > daysInMonth +dayOfWeek){
-                daysInMonthArray.add("");
-            }else{
-                daysInMonthArray.add(String.valueOf(i+dayOfWeek));
-            }
-        }
-        return daysInMonthArray;
-    }
-
-
-
-    private String monthYearFromDate(LocalDate date){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
-        return date.format(formatter);
-
-    }
-
-    public void previousMonthAction(View view){
-        selectedDate = selectedDate.minusMonths(1);
-        setMonthView();
-    }
-
-    public void nextMonthAction(View view){
-        selectedDate = selectedDate.plusMonths(1);
-        setMonthView();
-    }
-
-
-    @Override
-    public void onItemClick(int position, String dayText) {
-
-        if(dayText.equals("")){
-            String message = "Selected Date "+ dayText+" "+monthYearFromDate(selectedDate);
-            Toast.makeText(this,message,Toast.LENGTH_LONG).show();
-        }
-    }
 }
