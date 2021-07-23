4d2568613ef437462465bdfdff9aa5d195d96464
{milouw56@gmail.com} <milouw@naver.com>
2021-07-23 10:45:06 +0900

    뒤로가기 두번시 종료

--- app/src/main/java/com/example/bottonnav/MainActivity.java
+++ app/src/main/java/com/example/bottonnav/MainActivity.java
@@ -129,5 +129,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    private long backKeyPressedTime = 0;
+    private Toast toast;
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        // 기존 뒤로 가기 버튼의 기능을 막기 위해 주석 처리 또는 삭제
+
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간에 2.5초를 더해 현재 시간과 비교 후
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간이 2.5초가 지났으면 Toast 출력
+        // 2500 milliseconds = 2.5 seconds
+        if (System.currentTimeMillis() > backKeyPressedTime + 2500) {
+            backKeyPressedTime = System.currentTimeMillis();
+            toast = Toast.makeText(this, "뒤로 가기 버튼을 한 번 더 누르시면 종료됩니다.", Toast.LENGTH_LONG);
+            toast.show();
+            return;
+        }
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간에 2.5초를 더해 현재 시간과 비교 후
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간이 2.5초가 지나지 않았으면 종료
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2500) {
+            finish();
+            finishAffinity();
+            System.runFinalization();
+            System.exit(0);
+        }
+    }
 
 }
